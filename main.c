ed36e216c2776543e6dac33b9df084bf9fc13ba2
srenegado <scottstationary@gmail.com>
2024-11-19 11:46:33 -0800

    Add memory and registers

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <SDL.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
     SDL_CreateWindow("Chip-8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 64 * 16, 32 * 16, 0);
+
+    char mem[4096] = {0}; // main memory
+    char Vx[16] = {0}; // variable registers: V0 to VF
+    char I[2] = {0}; // index register
+    char delay_timer = {0};
+    char sound_timer = {0};
+    char PC[2] = {0}; // program counter
+    char stack[32] = {0}; // for storing up to 16 addresses
 
     while (true) {
         SDL_Event event;
